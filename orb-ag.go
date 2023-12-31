d62b34c3fd44b7dbee797a3540c4fdc6c678c937
Anthony Green <green@moxielogic.org>
2023-12-31 08:38:26 -0500

    Add license header. Formatting.

--- orb-ag.go
+++ orb-ag.go
@@ -1,7 +1,26 @@
-// orb-ag.go - Observe and Report Buddy
+// orb-ag.go - an Observe and Report Buddy
 //
 // Copyright (C) 2023 Anthony Green - green@moxielogic.com
 //
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the “Software”), to deal in the Software without
+// restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 package main
 
@@ -281,11 +300,11 @@ func monitorOutput(pid int, scanner *bufio.Scanner, compiledSignals []CompiledSi
 			}
 		}
 
-    if (is_stderr) {
-      fmt.Fprintln(os.Stderr, line)
-    } else {
-      fmt.Println(line)
-    }
+		if is_stderr {
+			fmt.Fprintln(os.Stderr, line)
+		} else {
+			fmt.Println(line)
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal("orb-ag error: Problem reading from pipe: ", err)
